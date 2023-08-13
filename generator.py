f9e4e750515672c22eee9e3edba3a6f26ba2983d
Jeremy Mullis <jeremy.mullis@gmail.com>
2023-08-13 18:03:42 -0500

    setup gunicorn

--- generator.py
+++ generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd8b09e8e957eff048224033574250f1ed8d2d92591ba1cc39aea66b6eba89c1
-size 938
+oid sha256:eb4619aa7ad26a083dd75619d04525b2d59c97b284ef16f71a650a6c667d8375
+size 945
---
+++ gunicorn_config.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e2bcc5855034c3fabab3f8da720a9b5090772a0e831cc8bd9f0519f8dcfe27ad
+size 34
