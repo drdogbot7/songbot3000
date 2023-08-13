78110c40f2e4e1b57a543214b90e0fd753ceb03e
Jeremy Mullis <jeremy.mullis@gmail.com>
2023-08-13 14:42:25 -0500

    add more training data

--- songbot.py
+++ songbot.py
@@ -1,28 +1,3 @@
-import argparse
-from generator import generate_song_titles
-
-parser = argparse.ArgumentParser(
-    prog="Song Title Bot 3",
-    description="AI to generate funny song titles"
-)
-
-parser.add_argument('-n', '--number', default=1, type=int)
-parser.add_argument('-p', '--prompt', default='')
-parser.add_argument('-t', '--temperature', default=1.0, type=float)
-# parser.add_argument('-s', '--save', action='store_true')
-args = parser.parse_args()
-
-song_titles = generate_song_titles(
-    number=args.number,
-    temperature=args.temperature,
-    prompt=args.prompt
-)
-
-outputFile = open("output/log.txt", "a")
-
-for song_title in song_titles:
-    outputFile.write("\n")
-    outputFile.write(song_title)
-    print(song_title)
-
-print(args)
+version https://git-lfs.github.com/spec/v1
+oid sha256:8e6d1eadfb08f1e05a17c276958720d4ed33cd08f63477ec25709a86d975fa62
+size 729
